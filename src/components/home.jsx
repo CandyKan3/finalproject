676ed5ec814a99f005645b1d070c71431c3fe0b1
root <bkane3@uncc.edu>
2020-03-31 17:07:36 +0000

    Added conditional rendering based off of symptoms selected

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -7,15 +7,54 @@ import './home.scss';
 export default class NavExample extends React.Component {
   constructor(props) {
     super(props);
-
+//      {/*Need 4 state variables, 3 booleans and one int */}
     this.state = {
 
+      drycough: false,
+      fever: false,
+      flu: false,
+      age: 0,
+      results:false,
+      risk:"high"
     };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit(){
+
+    if(this.state.drycough || this.state.fever || this.state.flu)
+    this.setState({
+      risk: "low",
+      results:true
+    })
+    if(this.state.drycough & this.state.flu || this.state.flu & this.state.fever || this.state.drycough & this.state.fever)
+    this.setState({
+      risk: "medium",
+      results:true
+
+    })
+    if(this.state.drycough & this.state.fever & this.state.flu)
+    this.setState({
+      risk: "high",
+      results:true
+    })
+
+
+  }
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.checked
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+    console.log("State set to "+ value);
   }
 
-
   render() {
-
+if(!this.state.results){
     return (
  <div className="App section">
 
@@ -26,24 +65,17 @@ export default class NavExample extends React.Component {
 <Row>
 <div class="column is-two-fifths">
 
-
 <h1 className="title" >COVID-19 Symptom Checker</h1>
-
-
-
-
 </div>
-<div className="column">
-<h1 class="title">
-      Please select your symptoms
-    </h1>
-
+  <div className="column">
+  <h1 class="title"> Please select your symptoms  </h1>
     <p class="subtitle">
     Consult a physician for a final opinion</p>
-
-
+    {/*Begin form here
+      TODO: Write event handlers to process form using state variables */}
+       <form>
     <label class="checkbox">
-    <input type="checkbox" />
+    <input type="checkbox" name="drycough" checked={this.state.drycough} onChange={this.handleInputChange} />
   <span className="testing">
     Dry Cough
 </span>
@@ -52,7 +84,7 @@ export default class NavExample extends React.Component {
   <br>
   </br>
   <label class="checkbox">
-  <input type="checkbox" />
+  <input type="checkbox" name="fever" checked={this.state.fever} onChange={this.handleInputChange} />
     <span className="testing">
   Mild fever
   </span>
@@ -60,7 +92,7 @@ export default class NavExample extends React.Component {
 <br>
 </br>
 <label class="checkbox">
-<input type="checkbox" />
+<input type="checkbox"  name="flu" checked={this.state.flu} onChange={this.handleInputChange}/>
   <span className="testing">
 Negative Flu Test
 </span>
@@ -84,10 +116,19 @@ Negative Flu Test
 
     <div class="buttons">
 
-      <a class="button is-primary align-right">Submit</a>
+      <a class="button is-primary align-right" onClick={this.handleSubmit}>Submit</a>
 
     </div>
+     </form>
 </div>
+
+</Row>
+<Row>
+<br>
+</br>
+<br>
+</br>
+<h4>Disclaimer: This site should not be used for medical advice and is for educaitonal purposes only</h4>
 </Row>
   </body>
         </React.Fragment>
@@ -95,4 +136,71 @@ Negative Flu Test
 
     );
   }
+  if(this.state.risk ==="high"){
+      return (
+        <React.Fragment>
+        <Row>
+        <div class="column is-two-fifths">
+
+        <h1 className="title">Risk Level: High</h1>
+        </div>
+        <div className="column" >
+        <progress class="progress is-danger" max="100">90%</progress>
+
+          <h1 className="title ">Please take the following steps:</h1>
+
+            <h1 className="title is-4"> - Contact your physician</h1>
+              <h1 className="title is-4"> - Self Quarantine</h1>
+          <a href="https://www.cdc.gov/coronavirus/2019-ncov/if-you-are-sick/steps-when-sick.html" className="title"> Please consult the CDC website for the latest information -></a>
+          </div>
+          </Row>
+
+        </React.Fragment>
+      );
+  }
+  if(this.state.risk ==="medium"){
+    return (
+      <React.Fragment>
+      <Row>
+      <div class="column is-two-fifths">
+
+      <h1 className="title">Risk Level: Medium</h1>
+      </div>
+      <div className="column" >
+      <progress class="progress is-warning" max="100">90%</progress>
+
+        <h1 className="title ">Please take the following steps:</h1>
+
+          <h1 className="title is-4"> - Contact your physician</h1>
+            <h1 className="title is-4"> - Self Quarantine</h1>
+        <a href="https://www.cdc.gov/coronavirus/2019-ncov/if-you-are-sick/steps-when-sick.html" className="title"> Please consult the CDC website for the latest information -></a>
+        </div>
+        </Row>
+
+      </React.Fragment>
+    );
+  }
+  if(this.state.risk ==="low"){
+    return (
+      <React.Fragment>
+      <Row>
+      <div class="column is-two-fifths">
+
+      <h1 className="title">Risk Level: Low</h1>
+      </div>
+      <div className="column" >
+      <progress class="progress is-success" max="100">90%</progress>
+
+        <h1 className="title ">Please take the following steps:</h1>
+
+          <h1 className="title is-4"> - Contact your physician</h1>
+            <h1 className="title is-4"> - Self Quarantine</h1>
+        <a href="https://www.cdc.gov/coronavirus/2019-ncov/if-you-are-sick/steps-when-sick.html" className="title"> Please consult the CDC website for the latest information -></a>
+        </div>
+        </Row>
+
+      </React.Fragment>
+    );
+  }
+}
 }
